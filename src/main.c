deb7e612061c52755187744a095d781ea44845b3
Duane F. King <duaneking@users.noreply.github.com>
2022-03-05 18:31:53 -0800

    Example simple lib and usage for C.

---
+++ src/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lib.h"
+
+#define MAX 25
+
+int a[MAX];
+
+void main(int argc, char *argv[])
+{
+    int i,t,x,y;
+
+    /* fill array */
+    for (i=0; i < MAX; i++)
+    {
+        a[i]=rand();
+
+        printf("%d\n",a[i]);
+    }
+
+    bubble_sort(MAX, a);
+
+    /* print sorted array */
+    printf("--------------------\n");
+
+    for (i=0; i < MAX; i++){
+        printf("%d\n",a[i]);
+    }
+}
